15852ae3855c5aa2d62eeb3a433587df7b313bd3
Jackson Souza <jackson.souza@kumulus.com.br>
2020-11-20 18:15:53 -0300

    adjust login screen

---
+++ lib/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+        ),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
